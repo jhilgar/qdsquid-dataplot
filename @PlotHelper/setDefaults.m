2929c86eba4824fd964b1952d5bead557112321a
jhilgar <jhilgar@ucsd.edu>
2020-03-20 16:10:44 -0700

    Stash destruction repairs

--- @PlotHelper/setDefaults.m
+++
@@ -1,8 +0,0 @@
-function setDefaults()
-    set(groot,'defaultAxesBox', PlotHelper.defaultAxesBox);
-    set(groot,'defaultLineLineWidth', PlotHelper.defaultLineLineWidth);
-    set(groot,'defaultAxesLineWidth', PlotHelper.defaultAxesLineWidth);
-    set(groot,'defaultAxesFontWeight', PlotHelper.defaultAxesFontWeight);
-    hold on;
-    axis square;
-end
