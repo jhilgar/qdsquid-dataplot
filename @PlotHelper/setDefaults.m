02d11383a754da798752225c53728643a350414f
jhilgar <jhilgar@ucsd.edu>
2020-03-20 15:50:42 -0700

    Stash

---
+++ @PlotHelper/setDefaults.m
@@ -0,0 +1,8 @@
+function setDefaults()
+    set(groot,'defaultAxesBox', PlotHelper.defaultAxesBox);
+    set(groot,'defaultLineLineWidth', PlotHelper.defaultLineLineWidth);
+    set(groot,'defaultAxesLineWidth', PlotHelper.defaultAxesLineWidth);
+    set(groot,'defaultAxesFontWeight', PlotHelper.defaultAxesFontWeight);
+    hold on;
+    axis square;
+end
