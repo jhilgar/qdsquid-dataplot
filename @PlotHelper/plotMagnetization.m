424ba5851716db95f7a0781e10865af4271fb1fd
jhilgar <jhilgar@ucsd.edu>
2020-03-20 15:18:20 -0700

    Stash madness

--- @PlotHelper/plotMagnetization.m
+++
@@ -1,8 +0,0 @@
-function plotMagnetization(obj)
-    xdata = obj.Parsed.Field;
-    ydata = obj.Parsed.Moment;
-
-    PlotHelper.plotDataset(xdata ./ 10000, ydata, obj.Parsed.TemperatureRounded, 'line', 1);
-    PlotHelper.setLimits();
-    xlabel('Field (T)'); ylabel('Magnetization (\mu_{B} mol^{-1})');
-end
