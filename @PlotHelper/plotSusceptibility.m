01bc7f4057d46c85b5273148d577a55116f41c43
jhilgar <jhilgar@ucsd.edu>
2020-03-20 15:50:02 -0700

    Stash

--- @PlotHelper/plotSusceptibility.m
+++
@@ -1,8 +0,0 @@
-function plotSusceptibility(obj)
-    xdata = obj.Parsed.Temperature;
-    ydata = obj.Parsed.ChiT;
-
-    PlotHelper.plotDataset(xdata, ydata, obj.Parsed.Field, 'scatter', 1, 'o');
-    PlotHelper.setLimits();
-    xlabel('Temperature (K)'); ylabel('\chiT (emu mol^{-1} K)');
-end
